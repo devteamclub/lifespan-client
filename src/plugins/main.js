bf6eeb672257eeb603a2e0f26b3147807ec427a0
i.tuev <sadiolem.sds@gmail.com>
2022-01-25 09:47:13 +0300

    up

--- src/plugins/main.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import AppButton from '@/components/AppButton'
-import AppInput from '@/components/AppInput'
-
-Vue.component('AppButton', AppButton)
-Vue.component('AppInput', AppInput)
