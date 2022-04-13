79315f61511c7cce616d2b102cf9f7842df01e75
kirill <klimonov.dev@gmail.com>
2022-04-13 21:23:25 +0300

    add credentials to api call

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -7,7 +7,8 @@ const publicApi = ax.create({
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
-  }
+  },
+  withCredentials: true
 })
 
 publicApi.interceptors.response.use(
