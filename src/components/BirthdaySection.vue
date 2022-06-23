3b1b6fae7d6dd6716a4bf9f69c2744066ec237c3
tastytea-github <tastytea.github@gmail.com>
2022-06-23 12:00:09 +0700

    [LC-52] Add birthday sections

--- src/components/BirthdaySection.vue
+++ src/components/BirthdaySection.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <slot />
+    <div class="wrapper">
+      <div class="card">
+        <div class="left-column">
+          <img class="icon" :src="icon" alt="image" />
+          <div class="title">
+            <slot name="title" />
+          </div>
+        </div>
+        <div class="progress">
+          <slot name="progress" />
+        </div>
+      </div>
+      <div class="content">
+        <slot name="content" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,11 +29,50 @@ export default {
       type: String,
       required: false,
       default: defaultIcon
-    },
-    title: {
-      type: String,
-      required: true
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    padding: 24px;
+    background: #fdfdfd;
+    border: solid 2px transparent;
+    border-radius: 16px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 6%), 0 1px 2px 0 rgb(0 0 0 / 6%);
+
+    .card {
+      align-items: center;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+
+      .left-column {
+        display: flex;
+        
+        .icon {
+          height: 48px;
+          width: 48px;
+        }
+
+        .title {
+          display: flex;
+          align-items: center;
+          box-sizing: border-box;
+          padding-left: 20px;
+          color: #26272a;
+          font-size: 18px;
+          font-weight: 500;
+          line-height: 1.33;
+          font-family: "Avenir Next Rounded Pro",Arial,Helvetica,sans-serif;
+        }
+      }
+    }
+
+    .content {
+      padding-top: 24px;
+      padding-left: 70px;
+    }
+  }
+</style>
--- src/views/Birthday.vue
+++ src/views/Birthday.vue
@@ -18,8 +18,89 @@
           </div>
         </div>
         <div class="sections">
-          <BirthdaySection title="Example">
-            BirthdaySection
+          <BirthdaySection class="section">
+            <div slot="title">Reveal messages from the past</div>
+            <div slot="progress">Revealed 2/3</div>
+            <div slot="content">
+              <div>You have 3 messages</div>
+              <ul>
+                <li>from yourself from May 12 2022</li>
+                <li>from your mother May 12 2011</li>
+                <li>from your schoolmate</li>
+                <li>Birthday presents 3</li>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Financial Pulse</div>
+            <div slot="progress">Executed 22/25</div>
+            <div slot="content">
+              <ul>
+                <li>Lending 8</li>
+                <li>Borrowing 10</li>
+                <li>Staking 4</li>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Query onchain records</div>
+            <div slot="progress">Revealed 2/3</div>
+            <div slot="content">
+              <ul>
+                <li>NFT transfers 85 </li>
+                <ul>
+                  <div>DAOs 4</div>
+                  <li>Voting 42</li>
+                </ul>
+                <li>Multisig 8</li>
+                <li>POAP 25 </li>
+                <li>Awards 25</li>
+                <li>Certificates 25</li>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Off-chain activities</div>
+            <div slot="content">
+              <ul>
+                <li>last year memories 45</li>
+                <li>juno activities 45/150</li>
+                <a href="#">Add past activities</a>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Social Networks activities</div>
+            <div slot="content">
+              <ul>
+                <li>Twitter. 12 / 185</li>
+                <li>Instagram  5 / 45</li>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Add a visual layer to the token art</div>
+            <div slot="content" style="overflow: hidden">
+              <div style="width: 124px; height: 124px; background-color: #44d7b6; border-radius: 16px; float: right;"></div>
+            </div>
+          </BirthdaySection>
+           <BirthdaySection class="section">
+            <div slot="title">Set new goals for the next year</div>
+            <div slot="content">
+              <ul>
+                <li>Goals 4</li>
+                <a href="#">Compare previous goals </a>
+              </ul>
+            </div>
+          </BirthdaySection>
+          <BirthdaySection class="section">
+            <div slot="title">Run future projection </div>
+            <div slot="content">
+              <ul>
+                <li>New data points 5 /15</li>
+                <a href="#">Add new data points </a>
+              </ul>
+            </div>
           </BirthdaySection>
         </div>
       </div>
@@ -107,8 +188,16 @@ export default {
       }
 
       .sections {
-        width: 800px;
-        margin: 0 auto;
+        width: 928px;
+        margin: 40px auto 0 auto;
+
+        .section {
+          margin-top: 24px;
+
+          &:first-child {
+            margin-top: 0;
+          }
+        }
       }
     }
   }
